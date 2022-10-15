e266102a891e786bc3b8c30bea4301ddb032896b
MurphySpider <arashgh.dev@gmail.com>
2022-10-15 16:48:25 +0330

    updated

--- app/src/androidTest/java/com/murphyspider/instasave/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/murphyspider/instasave/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.murphyapps.imediasave", appContext.packageName)
+        assertEquals("com.murphyspider.instasave", appContext.packageName)
     }
 }
